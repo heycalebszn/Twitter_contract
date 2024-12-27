5c08bbfb00c2171b195b888e718a3c9a0d169b2c
Caleb Kalejaiye <155694747+heycalebszn@users.noreply.github.com>
2024-12-27 10:08:09 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,7 @@ async function main() {
     console.log("Contract deployed to:", contract.address);
 
     main().catch((error) => {
-        console.error(error);
+        console.error(error)
         process.exitCode = 1;
     })
 }
